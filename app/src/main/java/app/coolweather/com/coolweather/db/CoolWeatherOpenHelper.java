a031d5900ebe9f8eb9d24b28c1abf382cb4daeb5
Lin JianCheng <1501362874@qq.com>
2016-10-22 18:20:38 +0800

    更正了错误

--- app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
@@ -10,11 +10,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
-    public static final String CREATE_PROVINCE="Create table Province(id integer primary key autoincrement,"+
+    public static final String CREATE_PROVINCE="create table Province(id integer primary key autoincrement,"+
             "province_name text,province_code text)";
-    public static final String CREATE_CITY="Create table City(id integer primary key autoincrement,"+
-            "city_name text,city_code text,province integer)";
-    public static final String CREATE_COUNTY="Create table County(id integer primary key autoincrement,"+
+    public static final String CREATE_CITY="create table City(id integer primary key autoincrement,"+
+            "city_name text,city_code text,province_id integer)";
+    public static final String CREATE_COUNTY="create table County(id integer primary key autoincrement,"+
             "county_name text,county_code text,city_id integer)";
 
 
