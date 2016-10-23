7ab0b394605f05f35f2894d0af45f46e32d5cb4b
Lin JianCheng <1501362874@qq.com>
2016-10-23 15:18:06 +0800

    修改了WeatherActivity

---
+++ app/src/main/java/app/coolweather/com/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,104 @@
+package app.coolweather.com.coolweather.activity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.LinearGradient;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import app.coolweather.com.coolweather.R;
+import app.coolweather.com.coolweather.util.HttpCallbackListener;
+import app.coolweather.com.coolweather.util.HttpUtil;
+import app.coolweather.com.coolweather.util.Utility;
+
+/**
+ * Created by Administrator on 2016/10/22.
+ */
+
+public class WeatherActivity extends Activity {
+
+    private LinearLayout weatherInfoLayout;
+    private TextView cityNameText;
+    private TextView publishText;
+    private TextView weatherDespText;
+    private TextView temp1Text;
+    private TextView temp2Text;
+    private TextView currentDateText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+        weatherInfoLayout=(LinearLayout)findViewById(R.id.weather_info_layout);
+        cityNameText=(TextView)findViewById(R.id.city_name);
+        publishText=(TextView)findViewById(R.id.publish_text);
+        weatherDespText=(TextView)findViewById(R.id.weather_desp);
+        temp1Text=(TextView)findViewById(R.id.temp1);
+        temp2Text=(TextView)findViewById(R.id.temp2);
+        currentDateText=(TextView)findViewById(R.id.current_date);
+        String weatherCode=getIntent().getStringExtra("weatherCode");
+        if(!TextUtils.isEmpty(weatherCode)){
+            publishText.setText("同步中....");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeather(weatherCode);
+        }else{
+            showWeather();
+        }
+
+
+    }
+    private  void queryWeather(String weatherCode){
+        String adress="http://www.weather.com.cn/data/cityinfo/"+weatherCode+".html";
+        queryFromServer(adress);
+    }
+
+    private void queryFromServer(final String adress) {
+        HttpUtil.sendHttpRequest(adress, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                Utility.handleWeatherResponse(WeatherActivity.this,response);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showWeather();
+                    }
+                });
+            }
+
+            @Override
+            public void onError(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishText.setText("同步失败");
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    private void showWeather() {
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText(prefs.getString("city_name",""));
+        temp1Text.setText(prefs.getString("temp1",""));
+        temp2Text.setText(prefs.getString("temp2",""));
+        weatherDespText.setText(prefs.getString("weather_desp",""));
+        publishText.setText("今天"+prefs.getString("publish_time","")+"发布");
+        currentDateText.setText(prefs.getString("current_date",""));
+        weatherInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+
+    }
+}
